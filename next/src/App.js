0361a7ee0ab9bb3bd7187bfefd6501b1840a42a3
Jatin-dev12 <jatin@iosandweb.net>
2024-01-23 14:26:48 +0530

    h

---
+++ next/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Jatin Sehgal    </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Chal Bhai Padle Kab Tak Ese Hi Rota Rhega
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
